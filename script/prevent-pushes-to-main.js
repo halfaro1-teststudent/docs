4f37aeb83e79f0c1ba48a101cdfb2a9f303cc4d9
Hector Alfaro <hectorsector@github.com>
2022-04-29 09:40:27 -0400

    addDDDDD

--- script/prevent-pushes-to-main.js
+++ script/prevent-pushes-to-main.js
@@ -7,20 +7,20 @@
 
 // [end-readme]
 
-import { execSync } from 'child_process'
+// import { execSync } from 'child_process'
 
-const productionBranch = 'main'
-const currentBranch = execSync('git symbolic-ref --short HEAD', { encoding: 'utf8' }).trim()
+// const productionBranch = 'main'
+// const currentBranch = execSync('git symbolic-ref --short HEAD', { encoding: 'utf8' }).trim()
 
-if (currentBranch === productionBranch) {
-  console.error('')
-  console.error(
-    `🤚 Whoa! Pushing to the ${productionBranch} branch has been disabled to prevent accidental deployments to production.`
-  )
-  console.error('')
-  console.error(
-    "If you're aware of the risks and really want to push to this branch, add --no-verify to bypass this check."
-  )
-  console.error('')
-  process.exit(1)
-}
+// if (currentBranch === productionBranch) {
+//   console.error('')
+//   console.error(
+//     `🤚 Whoa! Pushing to the ${productionBranch} branch has been disabled to prevent accidental deployments to production.`
+//   )
+//   console.error('')
+//   console.error(
+//     "If you're aware of the risks and really want to push to this branch, add --no-verify to bypass this check."
+//   )
+//   console.error('')
+//   process.exit(1)
+// }
